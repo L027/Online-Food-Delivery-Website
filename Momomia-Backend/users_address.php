e1ea54b7743693c2ed7a09cab1674c7e60960e6e
L027 <109790843+L027@users.noreply.github.com>
2023-05-31 17:07:36 +0530

    Add files via upload

--- Momomia-Backend/users_address.php
+++ Momomia-Backend/users_address.php
@@ -10,18 +10,17 @@ if ($conn->connect_error) {
  die("Connection failed: " . $conn->connect_error);
 }
 session_start();
-if(isset($_POST['submit']))
+
+if(isset($_GET['submit']))
 {	 
-	 $fullname=mysqli_real_escape_string($conn,$_POST['fullname']);
-	 $email =mysqli_real_escape_string($conn, $_POST['email']);
-     $message=mysqli_real_escape_string($conn,$_POST['message']);
-	 $sql = "INSERT INTO contacted_users(Fullname,Email,Message)  VALUES('$fullname','$email','$message')";
-     $result = mysqli_query($conn,$sql);
+	 $address=mysqli_real_escape_string($conn, $_GET['address']);
+	 $note = mysqli_real_escape_string($conn,$_GET['note']);
+	 $sql = "INSERT INTO users_address(Address,Note)  VALUES('$address','$note')";
+	  $result = mysqli_query($conn,$sql);
 	 }
      if($result) {
         header("Location:contactus.html");
     }
-     
+	 
 $conn->close();
 ?>
-
